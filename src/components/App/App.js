da9988117baffad4956ddd8ff1fd9b7e89707e5f
Hunter Edward <Hunter.scheel@outlook.com>
2020-06-10 15:38:51 -0500

    moved to props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,86 @@ import Calculate from '../Calculate/Calculate'
 import './App.css';
 
 class App extends Component {
+  state = {
+    totalDistance: 0,
+    input_miles: 0,
+    input_time: 0,
+    lastTime: 0,
+    lastMiles: 0,
+    longestMiles: 0,
+    longestTime: 0,
+    fastestMiles: 0,
+    fastestTime: 0,
+    fastestSpeed: 0
+  }
+  updateRuns = () => {
+    const miles = this.state.input_miles;
+    const time = this.state.input_time;
+    const speed = miles / (time / 60);
+    const totalDistance = this.state.totalDistance;
+
+    this.setState({
+      lastMiles: miles,
+      lastTime: time,
+      lastSpeed: speed,
+      totalDistance: (Number(totalDistance) + Number(miles))
+    });
+    if (miles > this.state.longestMiles) {
+      this.setState({
+        longestMiles: miles,
+        longestTime: time,
+        longestSpeed: speed
+      });
+    }
+    if (speed > this.state.fastestSpeed) {
+      this.setState({
+        fastestMiles: miles,
+        fastestTime: time,
+        fastestSpeed: speed
+      });
+    }
+  }
+  setTime = (event) => {
+    const val = event.target.value;
+    this.setState({
+      input_time: val
+    });
+  }
+  setMiles = (event) => {
+    const val = event.target.value;
+    this.setState({
+      input_miles: val,
+    })
+  }
   render() {
     return (
-      <div>
+      <div className='container'>
         <header>
           <h1>React Track Runner</h1>
         </header>
-        <Calculate />
+        <div className="container">
+          <div className='row col-12'>
+            <div className='col-12 col-md-4 mx-1'>
+              <input type='number' placeholder="miles" onChange={this.setMiles} />
+            </div>
+            <div className='col-12 col-md-4 mx-1'>
+              <input type='number' placeholder="time(minutes)" onChange={this.setTime} />
+            </div>
+            <div className='col-12 col-md-3 mx-1'>
+              <button className="btn btn-success" onClick={this.updateRuns}>Add Run</button>
+            </div>
+          </div>
+        </div>
+        <hr /><h2>MOST RECENT RUN</h2>
+        <Calculate distance={this.state.lastMiles} time={this.state.lastTime} />
+        <hr /><h2>LONGEST RUN</h2>
+        <Calculate distance={this.state.longestMiles} time={this.state.longestTime} />
+        <hr /><h2>FASTEST RUN</h2>
+        <Calculate distance={this.state.fastestMiles} time={this.state.fastestTime} />
+        <hr />
+        <h2>
+          TOTAL DISTANCE:{this.state.totalDistance}
+        </h2>
       </div >
     );
   }
--- src/components/Calculate/Calculate.js
+++ src/components/Calculate/Calculate.js
@@ -1,92 +1,15 @@
 import React, { Component } from 'react';
 
 class Calculate extends Component {
-    state = {
-        miles: 0,
-        time: 0,
-        lastTime: 0,
-        lastMiles: 0,
-        lastSpeed: 0,
-        longestMiles: 0,
-        longestTime: 0,
-        longestSpeed: 0,
-        fastestMiles: 0,
-        fastestTime: 0,
-        fastestSpeed: 0
-    }
-    calculateSpeed = (distance, time) => {
-        let speed = (60 / time) * distance;
-        return speed;
-    }
-    setLast = (event) => {
-        let miles = this.state.miles;
-        let time = this.state.time;
-        let speed = this.calculateSpeed(miles, time);
-        this.setState({
-            lastMiles: miles,
-            lastTime: time,
-            lastSpeed: speed
-        });
-        if (miles > this.state.longestMiles) {
-            this.setState({
-                longestMiles: miles,
-                longestTime: time,
-                longestSpeed: speed
-            });
-        }
-        if (speed > this.state.fastestSpeed) {
-            this.setState({
-                fastestMiles: miles,
-                fastestTime: time,
-                fastestSpeed: speed
-            });
-        }
-    }
-    setTime = (event) => {
-        let val = event.target.value;
-        this.setState({
-            time: val
-        });
-    }
-    setMiles = (event) => {
-        let val = event.target.value;
-        this.setState({
-            miles: val
-        })
-    }
+
     render() {
         return (
-            <div className="container">
-                <div className='row col-12'>
-                    <div className='col-12 col-md-4 mx-1'>
-                        <input type='number' placeholder="miles" onChange={this.setMiles} />
-                    </div>
-                    <div className='col-12 col-md-4 mx-1'>
-                        <input type='number' placeholder="time(minutes)" onChange={this.setTime} />
-                    </div>
-                    <div className='col-12 col-md-3 mx-1'>
-                        <button className="btn btn-success" onClick={this.setLast}>Add Run</button>
-                    </div>
-                </div>
-                <div className='row my-4'>
-                    <div className='col-12 h3'><u> LAST RUN</u> </div>
-                    <div className='col-12 col-sm-4'>TIME: {this.state.lastTime} min </div>
-                    <div className='col-12 col-sm-4'>DISTANCE: {this.state.lastMiles} miles </div>
-                    <div className='col-12 col-sm-4'>SPEED: {this.state.lastSpeed} mph </div>
-                </div>
-                <div className='row my-4'>
-                    <div className='col-12 h3'><u> LONGEST RUN</u></div>
-                    <div className='col-12 col-sm-4'>TIME: {this.state.longestTime} min </div>
-                    <div className='col-12 col-sm-4'>DISTANCE: {this.state.longestMiles} miles </div>
-                    <div className='col-12 col-sm-4'>SPEED: {this.state.longestSpeed} mph </div>
-                </div>
-                <div className='row my-4'>
-                    <div className='col-12 h3'><u> FASTEST RUN</u></div>
-                    <div className='col-12 col-sm-4'>TIME: {this.state.fastestTime} min </div>
-                    <div className='col-12 col-sm-4'>DISTANCE: {this.state.fastestMiles} miles </div>
-                    <div className='col-12 col-sm-4'>SPEED: {this.state.fastestSpeed} mph </div>
-                </div>
-            </div >
+
+            <div className='row my-4'>
+                <div className='col-12 col-sm-4'>TIME: {this.props.time} min </div>
+                <div className='col-12 col-sm-4'>DISTANCE: {this.props.distance} miles </div>
+                <div className='col-12 col-sm-4'>SPEED: {this.props.distance / (this.props.time / 60)} mph </div>
+            </div>
         );
     }
 }
